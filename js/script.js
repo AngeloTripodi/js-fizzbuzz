97fd2946584d87562c8eda9d0eefc8cf0047f169
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-23 19:22:19 +0100

    Add Bonus 1 and bonus 2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
+let numberToAppend= '';
+
+
 for( let i=1; i<101; i++)
 {
     let numero = i;
+    if ((i%3===0) && (i%5===0)){
+        numero ='FizzBuzz';
 
-    if ((numero%3===0) && (numero%5===0)){
-        numero='FizzBuzz';
     }
-    else if (numero%3===0) {
+    else if (i%3===0) {
         numero='Fizz'
     } 
-    else if(numero%5===0){
+    else if(i%5===0){
         numero='Buzz';
     }
-   
+ 
     console.log(numero);
 
-    const numberToAppend =`
-    <p>${numero}</p>
-    `;
     
-    
-    document.querySelector('.number').append(numberToAppend);
+
+    if (numero == 'FizzBuzz'){
+        numberToAppend +=`
+        <p class="color-blue">${numero}</p>
+        `;
+    }
+
+    else if(numero == 'Fizz'){
+        numberToAppend +=`
+        <p class="color-red">${numero}</p>
+        `;
+    }
+
+    else if(numero == 'Buzz'){
+        numberToAppend +=`
+        <p class="color-green">${numero}</p>
+        `;
+    }
+    else{
+        numberToAppend +=`
+        <p>${numero}</p>
+        `;
+    }
 }
+
+document.querySelector('.number').innerHTML = numberToAppend;
+
+
+
+
+
 
 
