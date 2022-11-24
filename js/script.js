e52db22dc797ac00e4c8c8148ff4e2d7c03ee76e
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-24 10:17:08 +0100

    clean code js

--- js/script.js
+++ js/script.js
@@ -6,41 +6,30 @@ for( let i=1; i<101; i++)
     let numero = i;
     if ((i%3===0) && (i%5===0)){
         numero ='FizzBuzz';
+        numberToAppend +=`
+        <p class="color-blue">${numero}</p>
+        `;
 
     }
     else if (i%3===0) {
         numero='Fizz'
-    } 
-    else if(i%5===0){
-        numero='Buzz';
-    }
- 
-    console.log(numero);
-
-    
-
-    if (numero == 'FizzBuzz'){
-        numberToAppend +=`
-        <p class="color-blue">${numero}</p>
-        `;
-    }
-
-    else if(numero == 'Fizz'){
         numberToAppend +=`
         <p class="color-red">${numero}</p>
         `;
-    }
-
-    else if(numero == 'Buzz'){
+    } 
+    else if(i%5===0){
+        numero='Buzz';
         numberToAppend +=`
         <p class="color-green">${numero}</p>
         `;
     }
     else{
         numberToAppend +=`
-        <p>${numero}</p>
-        `;
+       <p>${numero}</p>
+         `;
     }
+ 
+    console.log(numero);
 }
 
 document.querySelector('.number').innerHTML = numberToAppend;
